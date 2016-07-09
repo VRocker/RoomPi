6db562216bf588308c741857f523e019fb35a53d
VRocker <craigr3@hotmail.com>
2016-07-09 23:27:45 +0100

    Make sure we have valid pointers before trying to use them

--- doorsense/DoorStatePacket.cpp
+++ doorsense/DoorStatePacket.cpp
@@ -11,30 +11,33 @@ void DoorStatePacket::PreparePacket( void* packer, unsigned char packetID)
 void DoorStatePacket::State(bool open)
 {
 	msgpack_sbuffer* buffer = msgpack_sbuffer_new();
-	msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
-
-	PreparePacket(pk, (uint8_t)SensorPacketIDs::DoorStatus);
-
-	msgpack_pack_bool(pk, open);
-
-	buffer->data[buffer->size] = 0;
-
-	// Send the data off to the DataHandler process
-	ClientSock::GetSingleton()->Send(buffer->data, buffer->size);
-
-	// Stops ZeroMQ from stalling as it must handle a recv before continuing
-	ClientSock::GetSingleton()->Recv();
-
-	// Cleanup memory
 	if (buffer)
 	{
-		msgpack_sbuffer_free(buffer);
-		buffer = nullptr;
-	}
+		if (pk)
+		{
+			msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
 
-	if (pk)
-	{
-		msgpack_packer_free(pk);
-		pk = nullptr;
+			PreparePacket(pk, (uint8_t)SensorPacketIDs::DoorStatus);
+
+			msgpack_pack_bool(pk, open);
+
+			buffer->data[buffer->size] = 0;
+
+				// Send the data off to the DataHandler process
+			ClientSock::GetSingleton()->Send(buffer->data, buffer->size);
+
+				// Stops ZeroMQ from stalling as it must handle a recv before continuing
+			ClientSock::GetSingleton()->Recv();
+
+				// Cleanup memory
+	
+			msgpack_sbuffer_free(buffer);
+			buffer = nullptr;
+	
+
+	
+			msgpack_packer_free(pk);
+			pk = nullptr;
+		}
 	}
 }
--- tempsense/TempSensorPacket.cpp
+++ tempsense/TempSensorPacket.cpp
@@ -11,31 +11,31 @@ void TempSensorPacket::PreparePacket( void* packer, unsigned char packetID)
 void TempSensorPacket::TempAndHumidity(int temp, int humidity)
 {
 	msgpack_sbuffer* buffer = msgpack_sbuffer_new();
-	msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
-
-	PreparePacket(pk, (uint8_t)SensorPacketIDs::TempAndHumid);
-
-	msgpack_pack_int(pk, temp);
-	msgpack_pack_int(pk, humidity);
-
-	buffer->data[buffer->size] = 0;
-
-	// Send the data off to the DataHandler process
-	ClientSock::GetSingleton()->Send(buffer->data, buffer->size);
-
-	// Stops ZeroMQ from stalling as it must handle a recv before continuing
-	ClientSock::GetSingleton()->Recv();
-
-	// Cleanup memory
 	if (buffer)
 	{
-		msgpack_sbuffer_free(buffer);
-		buffer = nullptr;
-	}
+		msgpack_packer* pk = msgpack_packer_new(buffer, msgpack_sbuffer_write);
 
-	if (pk)
-	{
-		msgpack_packer_free(pk);
-		pk = nullptr;
+		if (pk)
+		{
+			PreparePacket(pk, (uint8_t)SensorPacketIDs::TempAndHumid);
+
+			msgpack_pack_int(pk, temp);
+			msgpack_pack_int(pk, humidity);
+
+			buffer->data[buffer->size] = 0;
+
+						// Send the data off to the DataHandler process
+			ClientSock::GetSingleton()->Send(buffer->data, buffer->size);
+
+						// Stops ZeroMQ from stalling as it must handle a recv before continuing
+			ClientSock::GetSingleton()->Recv();
+
+						// Cleanup memory
+			msgpack_sbuffer_free(buffer);
+			buffer = nullptr;
+
+			msgpack_packer_free(pk);
+			pk = nullptr;
+		}
 	}
 }
